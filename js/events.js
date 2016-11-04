4c8a5bb3b0b6a65a9bb79ac498aece7c04c52cb0
lshapz <laurashapiro01@gmail.com>
2016-11-04 14:07:16 -0400

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,45 @@
 //define functions here
+function getIt(){
+  $('p').on('click', function(){
+    alert('Hey!')
+  } 
+    )
+
+}
+
+function frameIt(){
+  $('img').on('load', function(){
+    $('img').addClass("tasty")
+
+  }
+    )
+}
+
+
+function pressIt(){
+  $('#typing').on('keydown', function(e){
+ if (e.which === 71) {
+    alert('you have pressed the g key')
+  }
+
+
+  })
+}
+
+function submitIt(){
+  $('form').on("submit", function(){
+    alert("Your form is going to be submitted now.")
+
+
+  })
+
+}
 
 $(document).ready(function(){
-
+getIt()
+pressIt()
+submitIt()
+frameIt()
 // call functions here
 
 });
